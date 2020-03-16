d707cdac18b3c94c480b3f713cfbfeaf3a5d3eea
bbangwon <bbangwon.chung@gmail.com>
2020-03-17 00:10:29 +0900

    WpfTreeView_1

---
+++ WpfTreeView/WpfTreeView/HeaderToImageConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfTreeView
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+
+            if (path == null)
+                return null;
+
+            var name = MainWindow.GetFileFolderName(path);
+
+            var image = "file.png";
+
+            if (string.IsNullOrEmpty(name))
+                image = "drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "folder-closed.png";
+            
+
+
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
